c66a1e75b7c4a25c6d0fd059eb7f517404ab9782
Sylwia Ignerowicz <jagsyl2@gmail.com>
2021-02-14 22:09:29 +0100

    Added eventStore Reader

--- DeliveryCompany.EventStore.Reader/Program.cs
+++ DeliveryCompany.EventStore.Reader/Program.cs
@@ -1,36 +1,40 @@
-﻿//using EventStore.Client;
-//using System;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
+﻿using EventStore.Client;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace DeliveryCompany.EventStore.Reader
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            const string stream = "EmailNotifications";
+namespace DeliveryCompany.EventStore.Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string stream = "Package-delivery-stream";
 
-//            var settings = EventStoreClientSettings
-//                .Create("esdb://127.0.0.1:2113?tls=false");
+            var settings = EventStoreClientSettings
+                .Create("esdb://127.0.0.1:2113?tls=false");
 
-//            using (var client = new EventStoreClient(settings))
-//            {
-//                client.SubscribeToStreamAsync(
-//                    stream,
-//                    EventArrived);
-//            }
-//        }
+            using (var client = new EventStoreClient(settings))
+            {
+                client.SubscribeToStreamAsync(
+                    stream,
+                    StreamPosition.End,
+                    EventArrived);
 
-//        private static Task EventArrived(
-//            StreamSubscription streamSubscription, 
-//            ResolvedEvent resolvedEvent, 
-//            CancellationToken cancellationToken)
-//        {
-//                var jsonData = Encoding.UTF8.GetString(resolvedEvent.Data.ToArray());
-//                Console.WriteLine(jsonData);
+                Console.WriteLine("Press any key to close...");
+                Console.ReadLine();
+            }
+        }
 
-//        }
-//    }
-//}
+        private async static Task EventArrived(
+            StreamSubscription streamSubscription,
+            ResolvedEvent resolvedEvent,
+            CancellationToken cancellationToken)
+        {
+            var jsonData = Encoding.UTF8.GetString(resolvedEvent.Event.Data.ToArray());
+            Console.WriteLine(jsonData);
+
+        }
+    }
+}
