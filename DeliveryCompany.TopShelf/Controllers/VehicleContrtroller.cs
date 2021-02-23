4e63e183dce7986466e92aba2f05a2e0c2980e63
Sylwia Ignerowicz <jagsyl2@gmail.com>
2021-02-23 21:15:18 +0100

    Added service by TopShelf - system adding to database does not work

---
+++ DeliveryCompany.TopShelf/Controllers/VehicleContrtroller.cs
@@ -0,0 +1,38 @@
+﻿using DeliveryCompany.BusinessLayer;
+using DeliveryCompany.DataLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DeliveryCompany.WebApi.Controllers
+{
+    [Route("api/vehicles")]
+    public class VehicleContrtroller : ControllerBase
+    {
+        private readonly IVehicleService _vehicleService;
+
+        public VehicleContrtroller(IVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        /*
+        Method: POST
+        URI: http://localhost:10500/api/vehicles
+        Body: 
+            {
+              "id": 0,
+              "mark": "Mercedes",
+              "model": "GLA",
+              "registrationNumber": "BA56421",
+              "loadCapacity": 400,
+              "averageSpeed": 110,
+              "driverId": 6
+            }
+        */
+        [HttpPost]
+        public async Task PostVehicleAsync([FromBody] Vehicle vehicle)
+        {
+            await _vehicleService.AddAsync(vehicle);
+        }
+    }
+}
